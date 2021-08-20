ff50b848544a4a0a95955d4d755ae607e5e86363
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-08-20 23:27:05 +0200

    attempt with wasm

--- wasm-simple/plugin-sample/src/lib.rs
+++ wasm-simple/plugin-sample/src/lib.rs
@@ -1,18 +1,8 @@
 use tremor_core::{MinFunction, MinBuilder};
 
-/// This is what the plugin publicly exports. It has to use `#[no_mangle]` so
-/// that its name is known when loading from the main binary.
-///
-/// The first approach exposes the function via a pointer, which is saved as a
-/// static variable.
-#[no_mangle]
-pub static with_static: MinFunction = min;
-
-fn min(a: i32, b: i32) -> i32 {
-    a.min(b)
-}
-
-/// Using `pub` is enough to export the function.
+/// Using `pub` is enough to export the function. In the case of Wasm, `static`
+/// isn't worth using to export the function, since globals can only be integers
+/// and it would require some more complicated conversion.
 #[no_mangle]
 pub fn with_extern(a: i32, b: i32) -> i32 {
     a.min(b)
--- wasm-simple/src/main.rs
+++ wasm-simple/src/main.rs
@@ -1,42 +1,35 @@
-use tremor_core::{MinFunction, MinBuilder, MinBuilderFunction};
+use tremor_core::{MinBuilder, MinBuilderFunction, MinFunction};
 
 use std::{env, process};
 
-use libloading::Library;
+use wasmer_runtime::{error::Error as WasmerError, imports, instantiate, Func, Global, Value};
 
 fn usage() {
     println!("Usage: ./main PLUGIN_PATH");
 }
 
-fn run_plugin(path: &str) -> Result<(), libloading::Error> {
-    unsafe {
-        println!("Running plugin");
-        let library = Library::new(path)?;
+static WASM: &'static [u8] =
+    include_bytes!("../plugin-sample/target/wasm32-unknown-unknown/release/plugin_sample.wasm");
 
-        println!("With static:");
-        let min_static = library.get::<*mut MinFunction>(b"with_static\0")?.read();
-        println!("  min(1, 2): {}", min_static(1, 2));
-        println!("  min(-10, 10): {}", min_static(-10, 10));
-        println!("  min(2000, 2000): {}", min_static(2000, 2000));
+fn run_plugin(_path: &str) -> Result<(), WasmerError> {
+    let import_object = imports! {};
+    let instance = instantiate(WASM, &import_object)?;
+    let context = instance.context();
 
-        println!("With extern:");
-        let min_extern = library.get::<MinFunction>(b"with_extern\0")?;
-        println!("  min(1, 2): {}", min_extern(1, 2));
-        println!("  min(-10, 10): {}", min_extern(-10, 10));
-        println!("  min(2000, 2000): {}", min_extern(2000, 2000));
+    println!("With extern:");
+    let min_extern: Func<(i32, i32), i32> = instance.exports.get("with_extern")?;
+    println!("  min(1, 2): {}", min_extern.call(1, 2)?);
+    println!("  min(-10, 10): {}", min_extern.call(-10, 10)?);
+    println!("  min(2000, 2000): {}", min_extern.call(2000, 2000)?);
 
-        println!("With extern + dyn:");
-        // Sample implementor of the trait that's dynamically dispatched
-        struct MinImplementor;
-        impl MinBuilder for MinImplementor {
-            fn min(&self, a: i32, b: i32) -> i32 { a.min(b) }
-        }
-        let builder = Box::new(MinImplementor {}) as Box<dyn MinBuilder>;
-        let min_extern_dyn = library.get::<MinBuilderFunction>(b"with_extern_dyn\0")?;
-        println!("  min(1, 2): {}", min_extern_dyn(&builder, 1, 2));
-        println!("  min(-10, 10): {}", min_extern_dyn(&builder, -10, 10));
-        println!("  min(2000, 2000): {}", min_extern_dyn(&builder, 2000, 2000));
-    }
+    println!("With extern:");
+    let ptr: Global = instance.exports.get("with_static")?;
+    let min_extern = context.memory(ptr.get().to_u128() as u32);
+    min_extern.view::<Func<(i32, i32), i32>>();
+    println!("what {:?}", min_extern);
+    // println!("  min(1, 2): {}", min_extern.call(1, 2)?);
+    // println!("  min(-10, 10): {}", min_extern.call(-10, 10)?);
+    // println!("  min(2000, 2000): {}", min_extern.call(2000, 2000)?);
 
     Ok(())
 }
@@ -51,7 +44,7 @@ fn main() {
         }
     };
 
-    if let Err(e) =  run_plugin(&path) {
+    if let Err(e) = run_plugin(&path) {
         eprintln!("Error when running the plugin: {}", e)
     }
 }
