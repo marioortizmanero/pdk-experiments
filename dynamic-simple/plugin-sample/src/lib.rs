96a450e382b8059f1966a1c10c981581cff2705a
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-09-04 13:20:45 +0200

    Fix dynamic example and remove inheritance for now

--- dynamic-simple/plugin-sample/src/lib.rs
+++ dynamic-simple/plugin-sample/src/lib.rs
@@ -1,4 +1,4 @@
-use common_ds::{MinFunction, MinBuilder};
+use common_ds::MinFunction;
 
 /// This is what the plugin publicly exports. It has to use `#[no_mangle]` so
 /// that its name is known when loading from the main binary.
@@ -14,29 +14,7 @@ pub extern "C" fn min(a: i32, b: i32) -> i32 {
 
 /// This is the second approach, where the function is directly exposed via
 /// dynamic linking, using the Rust ABI.
-///
-/// Using `dylib`, this is equivalent to using `extern "Rust" fn with_extern`.
-///
-/// Exporting functions is both possible with `extern` and `static`, but the
-/// former makes more sense for Rust-to-Rust FFI because it's just simpler and
-/// more straightforward.
 #[no_mangle]
 pub extern "C" fn with_extern(a: i32, b: i32) -> i32 {
     a.min(b)
 }
-
-/*
-/// Attempting to export a generic function in the shared library.
-///
-/// Note that generic functions must be mangled, so trying to set `#[no_mangle]`
-/// will raise a warning.
-pub fn with_extern_generics<M: MinBuilder>(builder: M, a: i32, b: i32) -> i32 {
-    builder.min(a, b)
-}
-
-/// Instead of using generics, trying with dynamic dispatching.
-#[no_mangle]
-pub fn with_extern_dyn(builder: &Box<dyn MinBuilder>, a: i32, b: i32) -> i32 {
-    builder.min(a, b)
-}
-*/
--- dynamic-simple/src/lib.rs
+++ dynamic-simple/src/lib.rs
@@ -1,4 +1,4 @@
-use common_ds::{MinBuilder, MinBuilderFunction, MinFunction};
+use common_ds::MinFunction;
 
 use libloading::Library;
 
@@ -18,25 +18,6 @@ pub fn run_plugin(path: &str) -> Result<(), libloading::Error> {
         println!("  min(1, 2): {}", min_extern(1, 2));
         println!("  min(-10, 10): {}", min_extern(-10, 10));
         println!("  min(2000, 2000): {}", min_extern(2000, 2000));
-
-        /*
-        println!("With extern + dyn:");
-        // Sample implementor of the trait that's dynamically dispatched
-        struct MinImplementor;
-        impl MinBuilder for MinImplementor {
-            fn min(&self, a: i32, b: i32) -> i32 {
-                a.min(b)
-            }
-        }
-        let builder = Box::new(MinImplementor {}) as Box<dyn MinBuilder>;
-        let min_extern_dyn = library.get::<MinBuilderFunction>(b"with_extern_dyn\0")?;
-        println!("  min(1, 2): {}", min_extern_dyn(&builder, 1, 2));
-        println!("  min(-10, 10): {}", min_extern_dyn(&builder, -10, 10));
-        println!(
-            "  min(2000, 2000): {}",
-            min_extern_dyn(&builder, 2000, 2000)
-        );
-        */
     }
 
     Ok(())
