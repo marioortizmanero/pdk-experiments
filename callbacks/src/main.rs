0fff80af3fb007b257b7bcc3f7847c4fccc1954d
Mario Ortiz Manero <marioortizmanero@gmail.com>
2022-02-10 14:08:09 +0100

    More callbacks experimenting

--- callbacks/src/main.rs
+++ callbacks/src/main.rs
@@ -1,65 +1,102 @@
-mod wrapper {
-    pub struct Sender<T> {
-        callback: fn(T),
-    }
-
-    impl<T> Sender<T> {
-        pub fn new(callback: fn(T)) -> Self {
-            Self {
-                callback
-            }
-        }
-
-        pub fn send(&self, t: T) {
-            (self.callback)(t);
-        }
-    }
-}
+const NUM_THREADS: i32 = 10;
 
 mod raw {
-    // This is common
-    pub struct ConnectorContext {
-        callback: fn(i32),
+    use super::NUM_THREADS;
+    use std::sync::atomic::{AtomicI32, Ordering};
+
+    // This type is shared by the plugin and the runtime
+    #[repr(C)]
+    struct ConnectorContext {
+        id: i32,
+        callback: extern "C" fn(i32),
     }
 
-    // This is the plugin
-    pub fn plugin_fn(ctx: &ConnectorContext) {
-        (ctx.callback)(1234);
+    // This is the plugin. It only invokes the callback with its own ID.
+    extern "C" fn plugin_fn(ctx: &ConnectorContext) {
+        (ctx.callback)(ctx.id);
     }
 
-    // This is the runtime
+    // This is the runtime. It will run a few plugins concurrently.
     pub fn run() {
-        let ctx = ConnectorContext {
-            callback: |x| println!("callback invoked v1! {}", x)
-        };
-        plugin_fn(&ctx);
+        // You still have access to some resources in the runtime, but don't
+        // abuse it.
+        static COUNT: AtomicI32 = AtomicI32::new(0);
+        extern "C" fn callback(x: i32) {
+            println!("raw callback invoked! {x:?}");
+            COUNT.fetch_add(1, Ordering::Relaxed);
+        }
+
+        let mut handles = Vec::new();
+        for id in 0..NUM_THREADS {
+            handles.push(std::thread::spawn(move || {
+                let ctx = ConnectorContext { id, callback };
+                plugin_fn(&ctx);
+            }))
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        println!("Final count: {}", COUNT.load(Ordering::Relaxed));
     }
 }
 
 mod nicer {
-    use super::wrapper::Sender;
+    mod wrapper {
+        pub struct Sender<T> {
+            callback: extern "C" fn(T),
+        }
 
-    // This is common
-    pub struct ConnectorContext {
-        sender: Sender<i32>
+        impl<T> Sender<T> {
+            pub fn new(callback: extern "C" fn(T)) -> Self {
+                Self { callback }
+            }
+
+            pub fn send(&self, t: T) {
+                (self.callback)(t);
+            }
+        }
     }
 
-    // This is the plugin
-    pub fn plugin_fn(ctx: &ConnectorContext) {
-        ctx.sender.send(1234);
+    use super::NUM_THREADS;
+    use wrapper::Sender;
+
+    // This type is shared by the plugin and the runtime
+    #[repr(C)]
+    struct ConnectorContext {
+        id: i32,
+        sender: Sender<i32>,
     }
 
-    // This is the runtime
+    // This is the plugin. It only invokes the callback with its own ID.
+    extern "C" fn plugin_fn(ctx: &ConnectorContext) {
+        ctx.sender.send(ctx.id);
+    }
+
+    // This is the runtime. It will run a few plugins concurrently.
     pub fn run() {
-        let ctx = ConnectorContext {
-            sender: Sender::new(|x| println!("callback invoked v2! {}", x))
-        };
-        plugin_fn(&ctx);
+        extern "C" fn callback(x: i32) {
+            println!("nicer callback invoked! {x}")
+        }
+
+        let mut handles = Vec::new();
+        for id in 0..NUM_THREADS {
+            handles.push(std::thread::spawn(move || {
+                let ctx = ConnectorContext {
+                    id,
+                    sender: Sender::new(callback),
+                };
+                plugin_fn(&ctx);
+            }))
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
     }
 }
 
-// TODO: will they work OK in a multi-threaded environment? Perhaps it needs a
-// mutex? Something?
 fn main() {
     raw::run();
     nicer::run();
